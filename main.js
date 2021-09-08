cbcc053a82d115bd39435be813af9633a6bb9109
dbaldew <dbaldew@gmail.com>
2021-09-08 21:19:34 +0200

    huiswerk

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("hallo")
+
+//event listener for search button
+document.getElementById("searchButton").addEventListener("click", queryInput);
+
+
+// function to get the query input
+function queryInput (){
+    let input = document.getElementById("input").value;
+    console.log(input)
+}
+
+//send query to server
+
+async function getQuery() {
+    try {
+        const response = await axios.get('https://restcountries.eu/rest/v2/');
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getQuery().then();
+
